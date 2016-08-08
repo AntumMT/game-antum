4ed6fd805979b4d744d855d78ff665c0c936b409
AntumDeluge <antumdeluge@gmail.com>
2016-08-07 20:54:33 -0700

    [stained_wood] Alphabetize color list.

--- mods/materials/stained_wood/init.lua
+++ mods/materials/stained_wood/init.lua
@@ -60,7 +60,7 @@ end
 
 -- Set stained wood colors
 wood_colors = {
-	"black", "blue", "brown", "gray", "green", "violet", "red",
+	"black", "blue", "brown", "gray", "green", "red", "violet",
 	"white", "yellow"}
 
 wood_colors_count = getWoodColorsCount(wood_colors)
