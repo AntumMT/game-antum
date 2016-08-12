4a234d5c52957e5e8611bdc02617a43131b8554c
AntumDeluge <antumdeluge@gmail.com>
2016-08-11 21:04:45 -0700

    [craft_guide] Register some aliases.

---
+++ mods/crafting/craft_guide/register_alias.lua
@@ -0,0 +1,5 @@
+
+minetest.register_alias("craft_guide", "craft_guide:sign_wall")
+minetest.register_alias("craftguide", "craft_guide:sign_wall")
+minetest.register_alias("craft_guide_pc", "craft_guide:lcd_pc")
+minetest.register_alias("craftguide_pc", "craft_guide:lcd_pc")
