d712a511914a7c19270657fe942bea546452ba3e
AntumDeluge <antumdeluge@gmail.com>
2016-09-03 13:16:56 -0700

    [creeper] spawn.lua: Try to reduce the spawn interval to every 20 minutes.

--- mods/mobs_aggressive/creeper/spawn.lua
+++ mods/mobs_aggressive/creeper/spawn.lua
@@ -1,8 +1,14 @@
+
+local time_sec = 1
+local time_min = time_sec * 60
+local time_hr = time_min * 60
+local time_day = time_hr * 24
+
 minetest.register_abm({
 	nodenames = {"default:dirt_with_grass","default:stone"},
 	neighbors = {"air"},
-	interval = 600,
-	chance = 500,
+	interval = time_min * 20, -- Run spawn function every 20 minutes
+	chance = 9000,
 	action = function(pos, node, _, active_object_count_wider)
 		if active_object_count_wider > 5 then
 			return
