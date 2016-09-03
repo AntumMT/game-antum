d2c1d2bf998d75049f8e383c0fd939579c1b184e
AntumDeluge <antumdeluge@gmail.com>
2016-09-02 21:40:54 -0700

    [mobs_monster] init.lua: Disable 'stone_monster' due to being graphic.

--- mods/mobs_aggressive/mobs_monster/init.lua
+++ mods/mobs_aggressive/mobs_monster/init.lua
@@ -16,7 +16,7 @@ dofile(path .. "/dirt_monster.lua") -- PilzAdam
 dofile(path .. "/dungeon_master.lua")
 dofile(path .. "/oerkki.lua")
 dofile(path .. "/sand_monster.lua")
-dofile(path .. "/stone_monster.lua")
+--dofile(path .. "/stone_monster.lua") -- DISABLED: Graphic
 dofile(path .. "/tree_monster.lua")
 dofile(path .. "/lava_flan.lua") -- Zeg9
 dofile(path .. "/mese_monster.lua")
