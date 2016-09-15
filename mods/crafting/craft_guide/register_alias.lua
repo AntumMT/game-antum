966872be741e22fd3b723bd1572e48d589550bce
AntumDeluge <antumdeluge@gmail.com>
2016-09-14 21:46:23 -0700

    [craft_guide] Remove 'register_alias.lua'.

--- mods/crafting/craft_guide/register_alias.lua
+++
@@ -1,5 +0,0 @@
-
-minetest.register_alias("craft_guide", "craft_guide:sign_wall")
-minetest.register_alias("craftguide", "craft_guide:sign_wall")
-minetest.register_alias("craft_guide_pc", "craft_guide:lcd_pc")
-minetest.register_alias("craftguide_pc", "craft_guide:lcd_pc")
