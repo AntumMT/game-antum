b5a4226673e4a65a620d11b081762e7c60abf2b4
AntumDeluge <antumdeluge@gmail.com>
2017-05-18 16:27:12 -0700

    [clean] Update to Git patch a1e150f:
    https://github.com/AntumDeluge/mtmod-clean/tree/a1e150f

--- mods/admin/clean/init.lua
+++ mods/admin/clean/init.lua
@@ -1,17 +1,29 @@
 -- LICENSE: WTFPL
 -- Copyright: PilzAdam
 
-local old_nodes = {"mod:a", "mod:b"}
+local old_nodes = {'mod:a', 'mod:b'}
 local old_entities = {'mobs_mc:skeleton', 'mobs_mc:creeper', 'kpgmobs:sheep'}
 
+-- Old/Missing nodes that should be replaced with something currently in game
+local replace_nodes = {
+    {'homedecor:bed_regular', 'homedecor:bed_white_regular'},
+}
+
+
+-- "Replaces" an old/non-existent node
+local function replace_node(old_node, new_node)
+    minetest.register_alias(old_node, new_node)
+end
+
+
 for _,node_name in ipairs(old_nodes) do
-    minetest.register_node(":"..node_name, {
+    minetest.register_node(':'..node_name, {
         groups = {old=1},
     })
 end
 
 minetest.register_abm({
-    nodenames = {"group:old"},
+    nodenames = {'group:old'},
     interval = 1,
     chance = 1,
     action = function(pos, node)
@@ -20,9 +32,14 @@ minetest.register_abm({
 })
 
 for _,entity_name in ipairs(old_entities) do
-    minetest.register_entity(":"..entity_name, {
+    minetest.register_entity(':'..entity_name, {
         on_activate = function(self, staticdata)
             self.object:remove()
         end,
     })
+end
+
+-- Replace old nodes
+for _, node_group in pairs(replace_nodes) do
+    replace_node(node_group[1], node_group[2])
 end
