85d62daee7752806d45f082a0896235538488422
AntumDeluge <antumdeluge@gmail.com>
2017-05-29 15:05:18 -0700

    [sneeker] Update to Git commit 258d366:
    https://github.com/AntumDeluge/mtmod-sneeker/tree/258d366

--- mods/mobiles/aggressive/sneeker/settings.lua
+++ mods/mobiles/aggressive/sneeker/settings.lua
@@ -1,7 +1,7 @@
 -- Settings for sneeker mod
 
 
-sneeker.debug = minetest.setting_get('sneeker.debug') or false
+sneeker.debug = minetest.setting_getbool('sneeker.debug') or false
 
 -- Maximum number of spawns active at one time
 sneeker.spawn_cap = minetest.setting_get('sneeker.spawn_cap') or 25
