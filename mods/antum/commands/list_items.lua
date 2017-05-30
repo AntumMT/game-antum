3fabb1b40eb2c600bd02ce6eba853a541fc6a0b8
AntumDeluge <antumdeluge@gmail.com>
2017-05-29 17:25:34 -0700

    [antum] modpack: Update to Git commit f8c0a34:
    https://github.com/AntumDeluge/mtmp-antum/tree/f8c0a34

--- mods/antum/commands/list_items.lua
+++ mods/antum/commands/list_items.lua
@@ -38,16 +38,80 @@ function antum.getRegisteredItemNames()
 end
 
 
+-- Compares a string from a list of substrings
+-- FIXME: Move to Antum functions
+function antum.compareSubstringList(ss_list, s_value)
+	for index, substring in ipairs(ss_list) do
+		-- Tests for substring (does not need to match full string)
+		if string.find(s_value, substring) then
+			return true
+		end
+	end
+	
+	return false
+end
+
+
+-- Checks if value is contained in list
+-- FIXME: Move to Antum functions
+function antum.listContains(tlist, v)
+	for index, value in ipairs(tlist) do
+		if v == value then
+			return true
+		end
+	end
+	
+	return false
+end
+
+
+-- Replaces duplicates found in a list
+-- FIXME: Move to Antum functions
+function antum.removeListDuplicates(tlist)
+	local cleaned = {}
+	for index, value in ipairs(tlist) do
+		if not antum.listContains(cleaned, value) then
+			table.insert(cleaned, value)
+		end
+	end
+	
+	return cleaned
+end
+
+
 antum.logAction('Registering chat command "listitems"')
 
 minetest.register_chatcommand('listitems', {
-	params = '',
+	params = '[string1] [string2] ...',
 	description = 'List registered items',
 	func = function(player, param)
-		local item_names = antum.getRegisteredItemNames()
-		if item_names ~= nil then
-			for INDEX in pairs(item_names) do
-				minetest.chat_send_player(player, item_names[INDEX])
+		-- Make all parameters lowercase for case-insensitive matching
+		param = antum.removeListDuplicates(string.split(string.lower(param), ' '))
+		
+		-- Debug
+		minetest.log('action', '[list_items] Parameters:')
+		for I in pairs(param) do
+			minetest.log('action', '  ' .. param[I])
+		end
+		
+		local all_item_names = antum.getRegisteredItemNames()
+		local found_names = {}
+		
+		-- Check if table is empty
+		if next(param) == nil then
+			found_names = all_item_names
+		else
+			-- Need to fill item list
+			for I in pairs(all_item_names) do
+				if antum.compareSubstringList(param, string.lower(all_item_names[I])) then
+					table.insert(found_names, all_item_names[I])
+				end
+			end
+		end
+		
+		if found_names ~= nil then
+			for I in pairs(found_names) do
+				minetest.chat_send_player(player, found_names[I])
 			end
 		else
 			minetest.chat_send_player(player, 'No registered items found!')
