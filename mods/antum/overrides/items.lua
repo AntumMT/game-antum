f98eee15a092ff16e90f76e38f771d132f935364
Jordan Irwin <antumdeluge@gmail.com>
2021-05-04 02:27:06 -0700

    Fix some overrides

--- mods/antum/overrides/items.lua
+++ mods/antum/overrides/items.lua
@@ -25,14 +25,18 @@
 --]]
 
 
-local modoverrides = {
-	'cooking',
-	'mobs',
-}
-
-for I in pairs(modoverrides) do
-	local modname = modoverrides[I]
-	if core.get_modpath(modname) then
-		antum.loadScript('items/' .. modname)
+local function replace_item(old, new)
+	if core.registered_items[old] and core.registered_items[new] then
+		core.log("action", "Replacing " .. old .. " with " .. new)
+		core.unregister_item(old)
+		core.register_alias(old, new)
 	end
 end
+
+replace_item("creatures:flesh", "mobs:meat_raw")
+replace_item("creatures:meat", "mobs:meat")
+replace_item("creatures:chicken_flesh", "mobs:chicken_raw")
+replace_item("creatures:chicken_meat", "mobs:chicken_cooked")
+replace_item("creatures:egg", "mobs:egg")
+replace_item("creatures:fried_egg", "mobs:chicken_egg_fried")
+replace_item("creatures:shears", "mobs:shears")
