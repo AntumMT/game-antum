6d7336f3ae653b2e9039c3b15c77f316c53fb729
Jordan Irwin <antumdeluge@gmail.com>
2021-05-05 18:00:57 -0700

    Remove duplicate craft recipe for farming:cotton

--- mods/antum/core/init.lua
+++ mods/antum/core/init.lua
@@ -1,19 +1,19 @@
 --[[ LICENSE HEADER
-  
+
   MIT License
-  
+
   Copyright © 2017 Jordan Irwin
-  
+
   Permission is hereby granted, free of charge, to any person obtaining a copy of
   this software and associated documentation files (the "Software"), to deal in
   the Software without restriction, including without limitation the rights to
   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
   of the Software, and to permit persons to whom the Software is furnished to do
   so, subject to the following conditions:
-  
+
     The above copyright notice and this permission notice shall be included in
     all copies or substantial portions of the Software.
-  
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,30 +21,20 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
-  
+
 --]]
 
 
 antum = {}
 antum.modname = core.get_current_modname()
 antum.modpath = core.get_modpath(antum.modname)
-antum.namespace = 'antum'
+antum.namespace = "antum"
 
 antum.verbose = false
-if core.settings:get_bool('log_mods') then
+if core.settings:get_bool("log_mods") then
 	antum.verbose = true
 end
 
 
 -- Load API functions first
-dofile(antum.modpath .. '/api.lua')
-
-
--- Other scripts to load
-local scripts = {
-	'crafting',
-}
-
-for index, script_name in pairs(scripts) do
-	antum.loadScript(script_name)
-end
+dofile(antum.modpath .. "/api.lua")
--- mods/antum/overrides/crafting/farming.lua
+++ mods/antum/overrides/crafting/farming.lua
@@ -1,19 +1,19 @@
 --[[ LICENSE HEADER
-  
+
   MIT License
-  
+
   Copyright © 2017 Jordan Irwin
-  
+
   Permission is hereby granted, free of charge, to any person obtaining a copy of
   this software and associated documentation files (the "Software"), to deal in
   the Software without restriction, including without limitation the rights to
   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
   of the Software, and to permit persons to whom the Software is furnished to do
   so, subject to the following conditions:
-  
+
     The above copyright notice and this permission notice shall be included in
     all copies or substantial portions of the Software.
-  
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,31 +21,23 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
-  
+
 --]]
 
 
-local cotton = {}
-cotton.dependencies = {"wool"}
-cotton.aliases = {"thread", "string"}
+local cotton_aliases = {"thread", "string"}
 
-cotton.dependencies.satisfied = false
-for dep in pairs(cotton.dependencies) do
-	if core.get_modpath(dep) then
-		cotton.dependencies.satisfied = true
-	end
-end
 
-if cotton.dependencies.satisfied then
-	-- Craft cotton from white wool
+if core.global_exists("farming") and core.get_modpath("wool") then
+	-- Craft cotton from wool
 	antum.registerCraft({
 		output = "farming:cotton 4",
 		type = "shapeless",
-		recipe = {"wool:white"},
+		recipe = {"group:wool"},
 	})
-	
+
 	-- Add aliases for cotton
-	for alias in pairs(cotton.aliases) do
+	for alias in pairs(cotton_aliases) do
 		core.register_alias("farming:" .. alias, "farming:cotton")
 	end
 end
