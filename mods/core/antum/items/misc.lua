ff726982e38cd3caa7a5161fde289d7d1b9ce496
Jordan Irwin <antumdeluge@gmail.com>
2021-05-25 02:27:07 -0700

    Consolidate core code

--- mods/core/antum/items/misc.lua
+++ mods/core/antum/items/misc.lua
@@ -14,3 +14,38 @@ if core.registered_items["bucket:bucket_water"] and core.registered_items["vesse
 		},
 	})
 end
+
+
+-- animalmaterials:contract
+core.register_craftitem(":ritems:contract", {
+	description = "Contract",
+	image = "ritems_contract.png",
+	stack_max=10,
+})
+
+if core.registered_items["animalmaterials:contract"] then
+	override.replaceItems("animalmaterials:contract", "ritems:contract")
+end
+
+if core.get_modpath("default") then
+	minetest.register_craft({
+		output = "ritems:contract",
+		recipe = {
+			{"default:paper"},
+			{"default:paper"},
+		}
+	})
+end
+
+-- animalmaterials:scale_*
+for _, color in pairs({"blue", "golden", "grey", "white"}) do
+	core.register_craftitem(":ritems:scale_" .. color, {
+		description = "Scale (" .. color .. ")",
+		image = "ritems_scale_" .. color .. ".png",
+		stack_max = 25
+	})
+
+	if core.registered_items["animalmaterials:scale_" .. color] then
+		override.replaceItems("animalmaterials:scale_" .. color, "ritems:scale_" .. color)
+	end
+end
--- mods/core/antum/items/tool.lua
+++ mods/core/antum/items/tool.lua
@@ -31,3 +31,71 @@ core.register_craft({
 })
 
 core.register_alias("animalmaterials:sword_deamondeath", "antum:sword_daemondeath")
+
+
+core.register_tool(":walking_light:helmet_gold", {
+	description = "Gold Helmet with light",
+	inventory_image = "walking_light_inv_helmet_gold.png",
+	wield_image = "3d_armor_inv_helmet_gold.png",
+	groups = {armor_head=10, armor_heal=6, armor_use=250},
+	wear = 0,
+})
+
+walking_light.addLightItem("walking_light", {
+	"helmet_gold",
+	}
+)
+
+
+antum.registerCraft({
+	output = "walking_light:helmet_gold",
+	recipe = {
+		{"default:torch"},
+		{"3d_armor:helmet_gold"},
+	}
+})
+
+
+local glowlight = {
+	cube = "homedecor:glowlight_small_cube_14",
+	quarter = "homedecor:glowlight_quarter_14",
+	half = "homedecor:glowlight_half_14",
+	glass = "moreblocks:super_glow_glass",
+}
+
+if core.registered_items[glowlight.glass] then
+	if core.registered_items[glowlight.cube] then
+		core.register_craft({
+			type = "shapeless",
+			output = glowlight.cube .. " 4",
+			recipe = {glowlight.glass},
+		})
+	end
+
+	if core.registered_items[glowlight.quarter] then
+		core.register_craft({
+			output = glowlight.quarter,
+			recipe = {
+				{glowlight.glass, glowlight.glass},
+			},
+		})
+	end
+
+	if core.registered_items[glowlight.half] then
+		core.register_craft({
+			output = glowlight.half,
+			recipe = {
+				{glowlight.glass, glowlight.glass},
+				{glowlight.glass, glowlight.glass},
+			},
+		})
+
+		core.register_craft({
+			output = glowlight.half,
+			recipe = {
+				{glowlight.quarter},
+				{glowlight.quarter},
+			},
+		})
+	end
+end
